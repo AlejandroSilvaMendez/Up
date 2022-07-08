e0fca85e235c6df46f1efca308ea796954fe0d19
SokyFre2 <108899509+SokyFre2@users.noreply.github.com>
2022-07-08 14:08:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -476,10 +476,10 @@ def onmessage(update,bot:ObigramClient):
         thread.store('msg',message)
 
         if '/start' in msgText:
-            start_msg = 'Bot          : @UploadFreBot\n'
+            start_msg = 'Bot : @UploadFreBot\n'
             start_msg+= 'Desarrollador: @SokyShop\n'
-            start_msg+=           : Enlaces que soporta Archivos en Mega Google Drive YouTube Enlaces de descarga directa\n'
-            start_msg+= 'Uso          : Envia Enlaces De Descarga y Archivos Para Procesar (Configure Antes De Empezar , Vea El /tutorial)\n'
+            start_msg+= 'Api: Enlaces que soporta Archivos en Mega Google Drive YouTube Enlaces de descarga directa\n'
+            start_msg+= 'Uso: Envia Enlaces De Descarga y Archivos Para Procesar (Configure Antes De Empezar , Vea El /tutorial)\n'
             bot.editMessageText(message,start_msg)
         elif '/files' == msgText and user_info['cloudtype']=='moodle':
              proxy = ProxyCloud.parse(user_info['proxy'])
